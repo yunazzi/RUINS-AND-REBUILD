feb71199fd0198640415277af12725ab78f28bd9
yunazzi <91316846+yunazzi@users.noreply.github.com>
2021-09-24 18:35:20 +0900

    Add files via upload

---
+++ js/sub.js
@@ -0,0 +1,33 @@
+$(function(){
+    //햄버거바 
+    $('.nav_menu .hamburbars').on('click',function(){
+        $(this).toggleClass('on');
+        $('.nav_menu').toggleClass('on');
+        $('.nav_menu').toggleClass('emphasized');
+    });
+    
+    // //이미지크기조절
+    // $("#post_content img, .resizablebox").each(function() {
+    //     var oImgWidth = $(this).width();
+    //     var oImgHeight = $(this).height();
+    //     $(this).css({
+    //         'max-width':oImgWidth+'px',
+    //         'max-height':oImgHeight+'px',
+    //         'width':'100%',
+    //         'height':'100%'
+    //     });
+    // });
+
+
+    //스크롤다운시 햄버거바 사라짐
+    var scrollsize = $('.nav_menu').height();
+    $(window).scroll(function(){  // 윈도우 스크롤 기능 작동
+        var hamburx = $(this).scrollTop() >= scrollsize; 
+        if(hamburx){
+            $('.nav_menu .hamburbars').css('display','none');
+        }
+        else{
+            $('.nav_menu .hamburbars').css('display','block');
+        }
+    });
+})
